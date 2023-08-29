a052855698aafab91d0dca6ba6c64438f98bedf5
altpfwlzh <altpfwlzh@gmail.com>
2023-08-29 17:08:16 +0900

    FEAT : 0141_Linked List Cycle

---
+++ leetCode/LinkedList/0141_Linked List Cycle.kt
@@ -0,0 +1,34 @@
+import java.io.*
+
+/*문제 및 설명 : https://yellow-shurup.tistory.com/entry/LeetCode0141Linked-List-CycleKotilnLinked-List*/
+
+class LinkedListCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        var slow : ListNode? = head
+        var fast : ListNode? = head
+
+        while(slow?.next != null && fast?.next != null) {
+            slow = slow?.next
+            fast = fast?.next?.next
+
+            if(slow == fast) return true
+        }
+        return false
+    }
+
+}
+
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+
+
+    val ans = LinkedListCycle().hasCycle(br.readLine())
+
+    bw.write(ans.toString())
+    bw.flush()
+    bw.close()
+
+}
