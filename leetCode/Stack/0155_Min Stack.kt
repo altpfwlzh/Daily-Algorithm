0b1b681f32ea90501b1183549ce15b24fad447e1
altpfwlzh <altpfwlzh@gmail.com>
2023-09-02 21:53:06 +0900

    FEAT : 0155_Min Stack

---
+++ leetCode/Stack/0155_Min Stack.kt
@@ -0,0 +1,47 @@
+package leetCode.Stack
+
+import java.io.*
+
+class MinStack {
+    var top: Node? = null
+
+    class Node(var data: Int, var min: Int, var prev: Node?)
+
+    fun push(x: Int) {
+        top = if (top == null) {
+            Node(x, x, null)
+        } else {
+            val min = Math.min(x, top!!.min)
+            Node(x, min, top)
+        }
+    }
+
+    fun pop() {
+        if (top == null) throw Exception("Empty Stack.")
+        top = if (top!!.prev == null) {
+            null
+        } else {
+            Node(top!!.prev!!.data, top!!.prev!!.min, top!!.prev!!.prev)
+        }
+    }
+
+    fun top(): Int {
+        if (top == null) throw Exception("Empty Stack.")
+        return top!!.data
+    }
+
+    fun getMin(): Int {
+        if (top == null) throw Exception("Empty Stack.")
+        return top!!.min
+    }
+
+}
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+fun main() {
+
+    bw.flush()
+    bw.close()
+}
