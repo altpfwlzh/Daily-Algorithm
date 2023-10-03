030c7e63667ae423d58da2122e5d80a1fbadee8f
altpfwlzh <altpfwlzh@gmail.com>
2023-10-03 21:51:51 +0900

    FEAT : 11399_ATM

--- Baekjun/Sort/11399_ATM.kt
+++ Baekjun/Sort/11399_ATM.kt
@@ -1,19 +1,31 @@
 package Baekjun.Sort
 
 /*
-* 문제 및 설명 : https://yellow-shurup.tistory.com/entry/Baekjoon11660%EA%B5%AC%EA%B0%84-%ED%95%A9-%EA%B5%AC%ED%95%98%EA%B8%B0-5KotlinPrefix
+* 문제 및 설명 : https://yellow-shurup.tistory.com/entry/Baekjoon11399ATMKotlin%EA%B5%AC%EA%B0%84%ED%95%A9
 * */
 
 import java.io.*
-
 private val br = BufferedReader(InputStreamReader(System.`in`))
 private val bw = BufferedWriter(OutputStreamWriter(System.out))
 
 fun main() {
-    val A : IntArray = br.readLine().split(" ").map { it.toInt() }.toIntArray()
 
-    A.sort()
+    val N : Int = br.readLine().toInt()
 
+    //입력 받고 정렬하기
+    val Pi : IntArray = br.readLine().split(" ").map { it.toInt() }.sorted().toIntArray()
+
+    val PiSum : IntArray = IntArray(N+1)
+    var minTime : Int = 0
+
+    //합배열 만들고 전체 합 출력 하기
+    for(i in 1 .. N){
+        PiSum[i] = PiSum[i-1] + Pi[i-1]
+        minTime += PiSum[i]
+    }
+
+    bw.write(minTime.toString())
+    bw.flush()
 
 
 }
