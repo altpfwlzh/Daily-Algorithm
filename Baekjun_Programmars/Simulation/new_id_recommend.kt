e9614c5bdc6c8de3f053a63591f97d48ea7bb2fc
altpfwlzh <altpfwlzh@gmail.com>
2026-02-23 18:14:09 +0900

    style : pro 신규 아이디 추천

--- Baekjun_Programmars/Simulation/new_id_recommend.kt
+++ Baekjun_Programmars/Simulation/new_id_recommend.kt
@@ -1,6 +1,6 @@
 package Baekjun_Programmars.Simulation
 
-private class Solution {
+private class Solution_new_id_recommend {
     fun solution(new_id: String): String {
         val step1 = new_id.lowercase()
         val step2 = step1.replace(Regex("[^a-z0-9-_.]"), "")
@@ -13,27 +13,28 @@ private class Solution {
         val answer: String = step7
         return answer
     }
-}
 
-private fun step2(s: String) {
-    val sb = StringBuilder()
-    for(c in s) {
-        val ok = (c in 'a'..'z') || (c in '0'..'9') || c == '-' || c == '_' || c == '.'
+    fun step2(s: String) {
+        val sb = StringBuilder()
+        for(c in s) {
+            val ok = (c in 'a'..'z') || (c in '0'..'9') || c == '-' || c == '_' || c == '.'
+        }
+
     }
 
-}
-
-private fun step3(s: String): String {
-    val sb = StringBuilder()
-    for(c in s) {
-        if ( c == '.' && sb.isNotEmpty() && sb.last() == '.') continue
-        sb.append(c)
+    fun step3(s: String): String {
+        val sb = StringBuilder()
+        for(c in s) {
+            if ( c == '.' && sb.isNotEmpty() && sb.last() == '.') continue
+            sb.append(c)
+        }
+        return sb.toString()
     }
-    return sb.toString()
+
+    fun step7(s: String): String {
+        var step7 = s
+        while (step7.length < 3) step7 += step7.last()
+        return step7
+    }
 }
 
-private fun step7(s: String): String {
-    var step7 = s
-    while (step7.length < 3) step7 += step7.last()
-    return step7
-}
